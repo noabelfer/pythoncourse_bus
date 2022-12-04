dc872b69aeb0ed16bccec4801bed39f27bfb0167
hbelfer <hbelfer@gmail.com>
2022-12-05 00:25:42 +0200

    add merge in place

--- merge/merge.py
+++ merge/merge.py
@@ -29,19 +29,36 @@ def merge2(num1:list, num2: list):
         out.append(num2.pop(0))
     return out
             
-    
+def merge3(num1:list, num2: list):
+    if(num1 == []):
+        for cu in num2:
+            num1.append(cu)
+        return num1 
+    while(len(num2) > 0):
+        ch = num2.pop(0)
+        for i ,v in enumerate(num1):
+            if(ch < v) or (v==0):
+                for j in range(len(num1)-1,i,-1):
+                    num1[j] = num1[j-1]
+                num1[i] = ch
+                break
+    return num1
+                
         
-print('Smple: ',merge(  [1,3,5,7,9,0,0,0],[2,4,6]))
+#print('Smple: ',merge(  [1,3,5,7,9,0,0,0],[2,4,6]))
 print('Smple2:',merge2([1,3,5,7,9,0,0,0],[2,4,6]))
+print('Smple3:',merge3([1,3,5,7,9,0,0,0],[2,4,6]))
 
-print('second  empty: ',merge([1,3,5,7,9,0,0],[]))
 print('second2 empty: ',merge2([1,3,5,7,9,0,0],[]))
+print('second3 empty: ',merge3([1,3,5,7,9,0,0],[]))
 
-print('third  empty: ',merge([],[1,2,3]))
+#print('third  empty: ',merge([],[1,2,3]))
 print('third2 empty: ',merge2([],[1,2,3]))
+print('third3 empty: ',merge3([],[1,2,3]))
 
-print('both  empty: ',merge([],[]))
-print('both2 empty: ',merge2([],[]))
+#print('both  empty: ',merge([],[]))
+#print('both2 empty: ',merge2([],[]))
 
-print('first negative:  ',merge([-9,-7,-5,-3,0,0,0],[2,4,6]))
+#print('first negative:  ',merge([-9,-7,-5,-3,0,0,0],[2,4,6]))
 print('first negative2: ',merge2([-9,-7,-5,-3,0,0,0],[2,4,6]))
+print('first negative3: ',merge3([-9,-7,-5,-3,0,0,0],[2,4,6]))
