4bb666829810b529f2a61d3fcb1ba0e31ae3f929
hbelfer <hbelfer@gmail.com>
2022-12-05 20:10:10 +0200

    make it work

--- ravkav/ravkavcls.py
+++ ravkav/ravkavcls.py
@@ -1,18 +1,29 @@
-import datetime
+from datetime import datetime
+import enum
 
+        
+class Rideoption(enum.Enum):
+    short = 'short'
+    medium = 'medium'
+    long = 'long'
+
+    
 class Ravkav:
-    holder_id=0
-    holder_name=""
-    balance = 0
-    logrides = [[],[]]  # list of touples (type,date)
+
+    class LOGR_IND(enum.Enum):
+        TYPE = 0
+        DATE = 1
+        
     def __init__(self,holder_id,holder_name):
         self.holder_id = holder_id
         self.holder_name = holder_name
+        self.balance = 0
+        self.logrides = [[],[]]  # list of touples (type,date)
         
     ravkav_dic = {
-        (0,40): {'name': 'short','price': 5.5},
-        (41,60):{'name': 'medium','price': 12},  
-        (61,1000):{'name': 'long','price': 23}} 
+        (0,40): {'name': Rideoption.short,'price': 5.5},
+        (41,60):{'name': Rideoption.medium,'price': 12},  
+        (61,1000):{'name': Rideoption.long,'price': 23}} 
         
     def display(self):
         print (self.ravkav_dic)
@@ -20,35 +31,43 @@ class Ravkav:
     def topup(self,shekels):
         self.balance += shekels
     
-    def getinfo(self,distance)->tuple:
+    def __getinfo(self,distance)->tuple:
         for rng  in self.ravkav_dic:
             if(distance >= rng[0] and distance <= rng[1]):
                 return (self.ravkav_dic[rng]['name'], self.ravkav_dic[rng]['price'])
                 
-    def ride(km,ridedate)->tuple:
-        info = getinfo(km)
-        if(info[0] > self.balance):
+    def ride(self,km:int,ridedate:str)->tuple:
+        d = datetime.strptime(ridedate, "%Y-%m-%d")
+        info = self.__getinfo(km)
+        if(info[1] > self.balance):
             return (False,info[1],info[0],self.balance)
-        self.logrides.append(info[0],info[0])
-        self.logrides.append(info[1],ridedate)
-        self.balance -= info[0]
+        self.logrides[self.LOGR_IND.TYPE.value].append(info[0])
+        self.logrides[self.LOGR_IND.DATE.value].append(d)
+        self.balance -= info[1]
         return (True,info[1],info[0],self.balance)
         
     def get_current_balance()->float:
         return self.balance
         
-    def get_rides_by_date(self,ridedate:datetime)->int:
-        return self.logrides[1].count(ridedate)
+    def get_rides_by_date(self,ridedate:str)->int:
+        d = datetime.strptime(ridedate, "%Y-%m-%d")
+        return self.logrides[1].count(d)
 
         
-    def get_rides_by_type(self,type:str)->int:
+    def get_rides_by_type(self,type:Rideoption)->int:
         return self.logrides[0].count(type)
               
 a = Ravkav(3,"moshe")
 a.display()
-a.topup(12)
+a.topup(300)
 a.display()
-b = a.getinfo(12)
-print(b)
-print('short count: ',a.get_rides_by_type('short'))
+a.ride(45,'2022-09-24')
+a.ride(46,'2022-09-24')
+a.ride(5,'2022-09-24')
+a.ride(26,'2022-09-24')
+
+print('short count: ',a.get_rides_by_type(Rideoption.short))
+print('medium count: ',a.get_rides_by_type(Rideoption.medium))
+print('long count: ',a.get_rides_by_type(Rideoption.long))
+print('date count: ',a.get_rides_by_date('2022-09-24'))
 
