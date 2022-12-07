7284182b49b8986ac80994fe491209a77c85a62f
hbelfer <hbelfer@gmail.com>
2022-12-07 22:13:39 +0200

    finish computations

--- csvaple/cmndline.py
+++ csvaple/cmndline.py
@@ -3,24 +3,39 @@
 import sys
 import csv
 
-infile = sys.argv[1]
+#infile = sys.argv[1]
+infile = "AAPL.csv"
 
 print(infile)
-
+LOW_COL = 1
+HIGH_COL = 4
+VOLUME_COL = 3
 with open(infile, newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-  
+    pre_year = 0
+    dict = {}
     for i,row in enumerate(spamreader):
-        if(i == 10):
-            break;
         if(i == 0):
             header = row
             print(len(header),header)
-            apledata = [[] for i in range(len(header))]
-            print(apledata)
         else:
-            for col in range(len(header)):
-                apledata[col].append(row[col])
-    
-    for i in range(len(header)):
-        print(apledata[i])
+            #Date,Low,Open,Volume,High,Close,Adjusted Close
+            # 0     1   2     3     4    5       6
+            year = row[0].split('-')[2]
+            if(year != pre_year):
+                dict[year] = {'lowpriice':  [],'highprice':[],'volume':[]}
+                pre_year = year
+            dict[year]['lowpriice'].append(float(row[LOW_COL]))
+            dict[year]['highprice'].append(float(row[HIGH_COL]))
+            dict[year]['volume'].append(float(row[VOLUME_COL]))
+
+    for y in dict.keys():
+        low_price  = min(dict[y]['lowpriice'])
+        high_price = max(dict[y]['highprice'])
+        ave_volume = sum(dict[y]['highprice'])/len(dict[y]['highprice'])
+        min_volume = min(dict[y]['highprice'])
+        max_volume = max(dict[y]['highprice'])
+        print('year: ',y,' low_p: ',low_price,' high_p: ',high_price,' ave_vol: ',ave_volume,' min_vol: ',min_volume,' max_vol: ',max_volume)
+        
+  
+ #   print(dict)
