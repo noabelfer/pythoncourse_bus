b692a2ff0810173ae02cdbb8c080a2db61f7e323
noabelfer <nbelfer@gmail.com>
2023-01-09 23:13:52 +0200

    cd

--- lesson_16/main.py
+++ lesson_16/main.py
@@ -1,28 +1,28 @@
-#!/usr/bin/env python
+#! /usr/local/bin/python3.11
 import argparse
-import vtapi
-import json
-from lesson_16 import *
+import vt
 
-default_api_key = '32cdd325e88f9126cf3fb455b301c3c17761e9b6c4c1fa4577255cedf069b74d'
-
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(
+    prog='virustotal scanner',
+    description = 'bla bla',
+    epilog = 'fefefwes')
 
 parser.add_argument('urls', nargs='+', help='List of urls to be scanned')
 parser.add_argument('-s', '--scan', action='store_true', help='Scan')
 parser.add_argument('--apikey', help='VTAPI user api key')
 
 args = parser.parse_args()
-print(args)
-
+print(args.urls, args.apikey, args.scan)
+#
 urls = args.urls
 toscan = args.scan
 apikey = args.apikey
 
 if (apikey == None):
-    apikey = default_api_key
+    apikey = "32cdd325e88f9126cf3fb455b301c3c17761e9b6c4c1fa4577255cedf069b74d"
+
 
 print('urls: '+str(urls)+' toscan: '+str(toscan) + ' apikey: '+str(apikey))
-# vt = vtapi.Vtapi(urls, apikey, toscan)
-# vt.run_as_threads()
-a = VT()
+a = vt.VT(urls,apikey)
+for i in range(len(urls)):
+    print(a.analysis_url(i))
--- lesson_16/vt.py
+++ lesson_16/vt.py
@@ -7,9 +7,12 @@ from datetime import timedelta, datetime, time
 import time
 import requests
 
+
+
+
 class VT:
-    def __init__(self, url:str, apikey):
-        self.url = url
+    def __init__(self, url: tuple, apikey):
+        self.urls:tuple = url
         self.apikey = apikey
         self.cache = {}
 
@@ -29,18 +32,18 @@ class VT:
             f.close()
 
 
-    def analysis_url(self) -> dict:
-
+    def analysis_url(self, url_indx:int) -> dict:
+        url = self.urls[url_indx]
         self.load_cache()
-        if (self.url in self.cache):
-            timeepoch = self.cache[self.url]['time']
+        if (url in self.cache):
+            timeepoch = self.cache[url]['time']
             time_analize = datetime.fromtimestamp(timeepoch)
             now = datetime.now()
             if now - timedelta(days=180) <= time_analize <= now:
                 print('getting the info from cache')
-                return(self.cache[self.url])
+                return(self.cache[url])
 
-        url_id = base64.urlsafe_b64encode(self.url.encode()).decode().strip("=")
+        url_id = base64.urlsafe_b64encode(url.encode()).decode().strip("=")
         url_base = "https://www.virustotal.com/api/v3/urls/"
         new_url = url_base + url_id
         headers = {"accept": "application/json",
@@ -49,25 +52,26 @@ class VT:
         # print(os.environ["VT_API"])
         response_anal = requests.get(new_url, headers=headers)
         if response_anal.status_code !=200 or response_anal.text is None:
-            if self.scan():
+            if self.scan(url):
                 return {}
             raise Exception('not existing url')
 
         file_anal = json.loads(response_anal.text)
-        data_url = ({'url': self.url,
+        data_url = ({'url': url,
         'reputation':file_anal.get("data").get("attributes").get("reputation"),
         'time': file_anal.get("data").get("attributes").get("last_analysis_date")})
 
 
-        self.cache[self.url]= data_url
+        self.cache[url]= data_url
         self.save_cache()
         print(data_url)
 
 
 
-    def scan(self) ->bool:
+    def scan(self, url_str:int) ->bool:
+        url = url_str
         url_vt = "https://www.virustotal.com/api/v3/urls"
-        payload = "url=" + self.url
+        payload = "url=" + url
         headers = {
             "accept": "application/json",
             "content-type": "application/x-www-form-urlencoded",
@@ -92,43 +96,20 @@ class VT:
     # n = analysis_url("https://cartasense-coldchain.com/")
     # print(n)
 
-if __name__ == '__main__':
-    try:
-         d = VT("bjkbjkbk","32cdd325e88f9126cf3fb455b301c3c17761e9b6c4c1fa4577255cedf069b74d")
-         print(d.analysis_url())
-         if d.analysis_url() == {}:
-             time.sleep(1)
-         print(d.analysis_url())
+# if __name__ == '__main__':
+#     try:
+#          d = VT("bjkbjkbk","32cdd325e88f9126cf3fb455b301c3c17761e9b6c4c1fa4577255cedf069b74d")
+#          print(d.analysis_url())
+#          if d.analysis_url() == {}:
+#              time.sleep(1)
+#          print(d.analysis_url())
+#
+#     except Exception as e:
+#         print (str(e))
 
-    except Exception as e:
-        print (str(e))
 
 
 
-#
-#     parser = argparse.ArgumentParser(
-#         prog='virustotal scanner',
-#         description = 'bla bla',
-#         epilog = 'fefefwes')
-#
-#     parser.add_argument('urls', nargs='+', help='List of urls to be scanned')
-#     parser.add_argument('-s', '--scan', action='store_true', help='Scan')
-#     parser.add_argument('--apikey', help='VTAPI user api key')
-#
-#     args = parser.parse_args()
-#     print(args.urls, args.apikey, args.scan)
-# #
-#     urls = args.urls
-#     toscan = args.scan
-#     apikey = args.apikey
-#
-# if (apikey == None):
-#     apikey = "32cdd325e88f9126cf3fb455b301c3c17761e9b6c4c1fa4577255cedf069b74d"
-#
-#
-
-    # print('urls: '+str(urls)+' toscan: '+str(toscan) + ' apikey: '+str(apikey))
-    #a = VT(urls[0],apikey)
     # a = VT("https://cartasense-coldchain.com/", "32cdd325e88f9126cf3fb455b301c3c17761e9b6c4c1fa4577255cedf069b74d" )
     # a.analysis_url()
     #
